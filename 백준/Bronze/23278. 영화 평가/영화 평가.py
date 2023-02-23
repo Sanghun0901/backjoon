4cbd86bee19584389a443405784eff3587135b1e
Sang Hun Lee <124578663+Sanghun0901@users.noreply.github.com>
2023-02-23 11:53:42 +0900

    [Bronze I] Title: 영화 평가, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/23278. 영화 평가/영화 평가.py
+++ 백준/Bronze/23278. 영화 평가/영화 평가.py
@@ -1,9 +1,5 @@
 N,L,H = map(int,input().split())
-if 1 <= N and 50 >= N and 0 <= L and 50 >= H and L+H < N:
-    pass
-else :
-    print('다시 입력하세요')
-
+  
 def up(x) :
     return x<=100
 S1 = list(map(int,input().split()))
@@ -18,7 +14,3 @@ if len(S) == N :
         S.pop(0)
 
     print(sum(S)/int(N-L-H))
-
-else :
-    print('다시 입력하세요')
-
