87e634ee2ccfcf0409d5723e5cb552fc5965423a
Sang Hun Lee <124578663+Sanghun0901@users.noreply.github.com>
2023-02-23 10:46:40 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,6 +1,4 @@
 def solution(array, commands):
-    answer = []
-    
-    [answer.append(sorted(array[i[0]-1:][:i[1]-i[0]+1])[i[2]-1]) for i in commands]
-           
+    answer = []   
+    [answer.append(sorted(array[i[0]-1:][:i[1]-i[0]+1])[i[2]-1]) for i in commands]           
     return answer
