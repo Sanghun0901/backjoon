0031f447f010eda7aa4b8949372aeb11b1fe041b
Sang Hun Lee <124578663+Sanghun0901@users.noreply.github.com>
2023-02-23 11:57:39 +0900

    [Bronze I] Title: 영화 평가, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/23278. 영화 평가/영화 평가.py
+++ 백준/Bronze/23278. 영화 평가/영화 평가.py
@@ -1,16 +1,15 @@
 N,L,H = map(int,input().split())
-  
+    
 def up(x) :
     return x<=100
-S1 = list(map(int,input().split()))
-S = list(filter(up,S1))
-if len(S) == N :
-    S.sort(reverse = True)
-    for _ in range(H):
-        S.pop(0)
+S = list(filter(up,list(map(int,input().split()))))
 
-    S.sort(reverse = False)
-    for _ in range(L):
-        S.pop(0)
+S.sort(reverse = True)
+for _ in range(H):
+    S.pop(0)
 
-    print(sum(S)/int(N-L-H))
+S.sort(reverse = False)
+for _ in range(L):
+    S.pop(0)
+
+print(sum(S)/int(N-L-H))
